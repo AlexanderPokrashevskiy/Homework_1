adfa6c6f3ca25ab892bc1a586151c28b39c85935
Alexander Pokrashevskiy <pokrashevskiy@gmail.com>
2020-10-27 00:55:02 +0300

    Homework task 1_2 done

---
+++ src/Homework_1_2.java
@@ -0,0 +1,24 @@
+public class Homework_1_2 {
+
+    public static void main(String[] args) {
+    //Создание переменных и инциализация значений
+
+        int a = 10;
+        System.out.println(a);
+        byte b = 22;
+        System.out.println(b);
+        short c = 120;
+        System.out.println(c);
+        long d = 22345;
+        System.out.println(d);
+        float e = 2.14f;
+        System.out.println(e);
+        double f = 3.14359;
+        System.out.println(f);
+        char g = 'g';
+        System.out.println(g);
+        boolean h = false;
+        System.out.println(h);
+
+    }
+}
