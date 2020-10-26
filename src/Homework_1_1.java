72847c010bb6808a3a90a5583fbba6b6d878f235
Alexander Pokrashevskiy <pokrashevskiy@gmail.com>
2020-10-27 01:57:27 +0300

    Homework task 1_4 done

--- src/Homework_1_1.java
+++ src/Homework_1_1.java
@@ -2,7 +2,5 @@ public class Homework_1_1 {
 
     public static void main(String[] args) {
 
-
-
     }
 }
---
+++ src/Homework_1_4.java
@@ -0,0 +1,15 @@
+public class Homework_1_4 {
+
+    public static void main(String[] args) {
+
+        int a = 10;
+        int b = 5;
+
+        if(a + b >= 10 && a + b <= 20) {
+            System.out.println(true);
+        }
+        else
+            System.out.println(false);
+
+    }
+}
