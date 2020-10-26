9dca3a132740b7758940bb5eadcfdee3e25e8478
Alexander Pokrashevskiy <pokrashevskiy@gmail.com>
2020-10-27 01:04:41 +0300

    Homework task 1_2 done

---
+++ src/Homework_1_3.java
@@ -0,0 +1,17 @@
+public class Homework_1_3 {
+
+    public static void main(String[] args) {
+
+        // Написать метод вычисляющий выражение a * (b + (c / d)) и возвращающий результат,где a, b, c, d – входные параметры этого метода;
+
+        int a = 10;
+        int b = 15;
+        int c = 30;
+        int d = 10;
+
+        int e = a * (b + (c / d));
+
+        System.out.println(e);
+
+    }
+}
