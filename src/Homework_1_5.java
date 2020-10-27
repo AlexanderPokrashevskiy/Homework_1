8e490d2b7819c670e3887c85b8bede1cfad60b03
Alexander Pokrashevskiy <pokrashevskiy@gmail.com>
2020-10-27 17:25:53 +0300

    Homework task 1_5 done

---
+++ src/Homework_1_5.java
@@ -0,0 +1,11 @@
+public class Homework_1_5 {
+
+
+    public static void main(String[] args) {
+
+        int a = 10;
+        if(a>=0) System.out.println("Введенное число: " + a + " - положительное");
+        else  System.out.println("Введенное число: " + a + " - отрицательное");
+    }
+
+}
