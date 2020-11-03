7f42d97b66a855219b23df142f0525a3856a5086
Alexander Pokrashevskiy <pokrashevskiy@gmail.com>
2020-11-04 01:15:45 +0300

    Homework task corrected

---
+++ src/Homework_2.java
@@ -0,0 +1,65 @@
+import java.sql.SQLOutput;
+import java.util.Arrays;
+
+public class Homework_2 {
+
+    public static void main(String[] args) {
+
+        task01();
+        task02();
+        task03();
+        task04();
+
+    }
+
+    // 1. Задать целочисленный массив, состоящий из элементов 0 и 1. Например: [ 1, 1, 0, 0, 1, 0, 1, 1, 0, 0 ]. С помощью цикла и условия заменить 0 на 1, 1 на 0;
+
+    public static void task01() {
+
+
+        int[] arr = {1, 1, 0, 0, 1, 0, 1, 1, 0, 0};
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == 1)
+                arr[i] = 0;
+            else arr[i] = 1;
+        }
+        System.out.println(Arrays.toString(arr));
+
+    }
+
+    // 2. Задать пустой целочисленный массив размером 8. С помощью цикла заполнить его значениями 0 3 6 9 12 15 18 21;
+
+    public static void task02() {
+
+        int[] arr = new int[8];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = i * 3;
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+
+    // 3. Задать массив [ 1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1 ] пройти по нему циклом, и числа меньшие 6 умножить на 2;
+
+    public static void task03() {
+
+        int[] arr = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] < 6) arr[i] *= 2;
+        }
+        System.out.print(Arrays.toString(arr));
+    }
+
+
+    // 4. Создать квадратный двумерный целочисленный массив (количество строк и столбцов одинаковое), и с помощью цикла(-ов) заполнить его диагональные элементы единицами;
+
+    public static void task04() {
+
+        int[][] arr = new int[10][10]
+        for (int i = 0; i < arr.length; i++) {
+
+        }
+
+    }
+}
